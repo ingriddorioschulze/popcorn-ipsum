90b89a6d362319bf0ba4420b9e20a19fedcf40e8
Ingrid do Rio Schulze <ingriddorio@gmail.com>
2019-08-26 19:49:54 +0200

    initial commit

---
+++ src/containers/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "../containers/App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Popcorn Ipsum</h1>
+      </div>
+    );
+  }
+}
+  
+export default App;
