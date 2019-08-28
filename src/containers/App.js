1eb8205220d65fefcd58378dbc1a06783a36748f
Ingrid do Rio Schulze <ingriddorio@gmail.com>
2019-08-28 10:17:38 +0200

    build the project structure and start paragraphs text

--- src/containers/App.js
+++ src/containers/App.js
@@ -5,7 +5,17 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Popcorn Ipsum</h1>
+        <div className="title">Popcorn Ipsum</div>
+        <div className="subtitle">Puff up your projects with the popcorn lorem ipsum.</div>
+        <div>How many paragraphs you need?</div>
+          <select name="number-paragraphs">
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+          </select>
+        <button>Pop!</button>
       </div>
     );
   }
---
+++ src/popcorn-text.js
@@ -0,0 +1,12 @@
+export const popcornText = [
+
+{
+    oneParagraph: "Popcorn ipsum dolor sit amet, . Chicken popcorn. Tasty popcorn. Healthy popcorn. Hot popcorn. Caramel popcorn. Chocolate popcorn. Butter popcorn. Cinema popcorn. Cheese popcorn. Sweet popcorn. Salty popcorn. Gourmet popcorn. Bacon popcorn, Fresh popcorn. Spicy Popcorn. Rainbow popcorn. Cinnamon popcorn. Salt and Vinegar popcorn. Cheddar popcorn. Garlic popcorn. Parmesan popcorn. Peanut Butter popcorn. Nutella popcorn. Microwave popcorn."
+},
+
+{
+    twoParagraphs: " "
+
+},
+
+]
